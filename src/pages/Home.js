5742f74861c38794e7f54687123899d89d75e7a7
saxenanihal95 <saxenanihal95@gmail.com>
2020-12-30 01:29:20 +0530

    Added patients table

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
+import patients from "../static/patients";
+
+import { createServer } from "miragejs";
+import { Table } from "antd";
+
+let server = createServer();
+server.get("/api/patients", {
+  patients,
+});
 
 export default function Home() {
-  return <div></div>;
+  const [patients, setPatients] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/patients")
+      .then((res) => res.json())
+      .then((json) => {
+        setPatients(json.patients);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+      });
+  }, []);
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Gender",
+      dataIndex: "gender",
+      key: "gender",
+    },
+  ];
+
+  return <Table dataSource={patients} columns={columns} loading={loading} />;
 }
