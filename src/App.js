6a2f1de7f42f41e6fc2905d3aee72f63caada650
saxenanihal95 <saxenanihal95@gmail.com>
2020-12-30 01:03:57 +0530

    created context for auth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+export const AuthContext = React.createContext();
+const initialState = {
+  isAuthenticated: !!localStorage.getItem("loggedInUser"),
+  loggedInUser: JSON.parse(localStorage.getItem("loggedInUser")),
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      localStorage.setItem(
+        "loggedInUser",
+        JSON.stringify(action.payload.loggedInUser)
+      );
+      return {
+        ...state,
+        isAuthenticated: true,
+        loggedInUser: action.payload.loggedInUser,
+      };
+    case "LOGOUT":
+      localStorage.removeItem("loggedInUser");
+      return {
+        ...state,
+        isAuthenticated: false,
+        loggedInUser: null,
+      };
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-        </Switch>
-      </Router>
+      <AuthContext.Provider value={{ state, dispatch }}>
+        <Router>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </Router>
+      </AuthContext.Provider>
     </div>
   );
 }
